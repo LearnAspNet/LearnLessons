66ac1e34e1ad6da54f50ec5762287ce632b5864e
minaev.maxim <mminmax@gmail.com>
2022-10-12 16:24:17 +0300

    [Lesson-3] Добавил материалы по теме.

---
+++ Lesson 3/src/LessonTwo.Api/LessonTwo.Api/Controllers/BookController.cs
@@ -0,0 +1,15 @@
+﻿using LessonTwo.Core.App.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LessonTwo.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookController : ControllerBase
+    {
+        public async Task<IActionResult> GetBooks()
+        {
+            return await Task.FromResult(Ok(BookLibraryService.BooksLibrary));
+        } 
+    }
+}
