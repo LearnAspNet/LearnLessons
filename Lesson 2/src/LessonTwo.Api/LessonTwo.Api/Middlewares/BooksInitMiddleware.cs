b4eeaf571ba0bc49316f352fa0a69b4c6e573e8d
Viktor <xjennyx068@gmail.com>
2022-10-24 09:55:06 +0300

    [LESN-2] привязался к свойству BookLibrary. далее через метода SetValue добавил 5 книг. Надеюсь, что правильно. Единственное, что не до конца понял, должен ли я в контексте данного задания именно заполнить содержимое книг или достаточно просто создать объекты

--- Lesson 2/src/LessonTwo.Api/LessonTwo.Api/Middlewares/BooksInitMiddleware.cs
+++ Lesson 2/src/LessonTwo.Api/LessonTwo.Api/Middlewares/BooksInitMiddleware.cs
@@ -16,20 +16,11 @@ namespace LessonTwo.Api.Middlewares
         public async Task InvokeAsync(HttpContext context)
         {
             Type myType = typeof(BookLibraryService);
-            Book book = new Book();
+            
+            var bookLibraryProp = myType.GetProperty("BookLibrary");
 
-            var nameBookProp = myType.GetProperty("Name");
-            var titleBookProp = myType.GetProperty("Title");
-            var registrationNumberProp = myType.GetProperty("RegistrationNumber");
-
-            var name = new List<string>() { "Name1", "Name2", "Name3", "Name4", "Name5" };
-            var title = new List<string>() { "Title1", "Title2", "Title3", "Title4", "Title5" };
-
-            for(int i = 0; i < 5; i++)
-            {
-                nameBookProp?.SetValue(book, $"{name[i]}");
-                titleBookProp?.SetValue(book, $"{title[i]}");
-            }
+            bookLibraryProp?.SetValue
+                (BookLibraryService.BooksLibrary, new List<Book>() { new Book(), new Book(), new Book(), new Book(), new Book() });
            
             await _next.Invoke(context);
             
